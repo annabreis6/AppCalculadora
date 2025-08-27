9ed3878fdd38624c8751fabb71987fb3a23c608b
annabreis6 <anna_b_reis@estudante.sesisenai.org.br>
2025-08-27 16:20:52 -0300

    implementação de teste

--- src/AppCalculadora.java
+++ src/AppCalculadora.java
@@ -1,5 +1,8 @@
 public class AppCalculadora {
     public static void main(String[] args) throws Exception {
-
+        System.out.println("Soma: " + calculadora.soma(10, 5));
+    	System.out.println("Subtração: " + calculadora.subtracao(10, 5));
+    	System.out.println("Multiplicação: " + calculadora.multiplicacao(10, 5));
+    	System.out.println("Divisão: " + calculadora.divisao(10, 5));
     }
 }
